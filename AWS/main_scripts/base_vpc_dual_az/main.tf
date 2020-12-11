d933c4e9b553aaf5b981e66d401b3dce92a15f45
Mike Wooten <mwooten@fortinet.com>
2020-12-11 09:41:20 -0600

    removed provider version statements from other main_script templates

--- AWS/main_scripts/base_vpc_dual_az/main.tf
+++ AWS/main_scripts/base_vpc_dual_az/main.tf
@@ -1,7 +1,6 @@
 
 provider "aws" {
   region     = var.aws_region
-  version    = "~> 3.0"
 }
 
 
--- AWS/main_scripts/deploy_ha_pair_standalone_existing_vpc/main.tf
+++ AWS/main_scripts/deploy_ha_pair_standalone_existing_vpc/main.tf
@@ -1,9 +1,6 @@
 
 provider "aws" {
   region     = var.aws_region
-  access_key = var.access_key
-  secret_key = var.secret_key
-  version    = "~> 3.0"
 }
 
 data "template_file" "fgt_userdata_byol1" {
